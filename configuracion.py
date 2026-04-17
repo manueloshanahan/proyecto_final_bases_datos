ff3607d3b8552375edcf6cf92b08797f6cec0f2f
Lucia Raventos <202410237@alu.comillas.edu>
2026-04-17 20:32:33 +0200

    s

--- configuracion.py
+++ configuracion.py
@@ -23,6 +23,6 @@ num_usuarios_y_similitudes = 30 #definimos aquí el valor por si posteriormente 
 #RUTAS DE LOS FICHEROS
 ruta_juegos = "C:/Users/manut/data/Video_Games_5.json"
 ruta_juguetes = "C:/Users/manut/data/Toys_and_Games_5.json"
-ruta_musica = ""C:/Users/lucia/OneDrive/Escritorio/2 iMat/2cuatri/BBDD/practicasproyecto\data\Digital_Music_5.json""
+ruta_musica = "C:/Users/manut/data/Digital_Music_5.json"
 ruta_instrumento = "C:/Users/manut/data/Musical_Instruments_5.json"
 ruta_alimentos = "C:/Users/manut/data/Grocery_and_Gourmet_Food_5.json"
